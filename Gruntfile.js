851358bd9d9633afe94b685f7c98fec92a7f8ffe
toporny <toporny@tlen.pl>
2016-04-02 14:44:47 +0100

    add build.bat and git ignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,9 +6,9 @@ module.exports = function (grunt) {
 	    files: [
 			// includes files within path
 			{expand: false, src: ['bower_components/jquery/dist/jquery.min.js'], dest: 'public_html/js/jquery.min.js', filter: 'isFile'},
-			{expand: false, src: ['bower_components/bootstrap/dist/bootstrap.min.js'], dest: 'public_html/js/bootstrap.min.js', filter: 'isFile'},
+			{expand: false, src: ['bower_components/bootstrap/dist/js/bootstrap.min.js'], dest: 'public_html/js/bootstrap.min.js', filter: 'isFile'},
 			{expand: false, src: ['bower_components/bootstrap/dist/css/bootstrap.min.css'], dest: 'public_html/css/bootstrap.min.css', filter: 'isFile'},
-			{expand: false, src: ['bower_components/parsleyjs/src/parsley.js'], dest: 'public_html/js/parsley.js', filter: 'isFile'},
+			{expand: false, src: ['bower_components/parsleyjs/dist/parsley.js'], dest: 'public_html/js/parsley.js', filter: 'isFile'},
 			{expand: false, src: ['bower_components/parsleyjs/dist/parsley.css'], dest: 'public_html/css/parsley.css', filter: 'isFile'},
 	    ],
 	  },
